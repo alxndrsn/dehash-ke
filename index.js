b8f01c0a5c84e7f5a8f9624e39df4a789c1ba89d
alxndrsn <alxndrsn>
2024-04-26 09:58:28 +0000

    Node module

---
+++ index.js
@@ -0,0 +1,16 @@
+module.exports = async hash => {
+  if(!/^[a-z0-9]{64}$/.test(hash)) throw new Error('Unrecognised format.  Expected a sha256 hash.');
+
+  const pref = hash.substr(0, 4);
+  const rest = hash.substr(4);
+
+  const res = await fetch(`https://alxndrsn.github.io/dehash-ke/v1/phone/sha256/${pref}.json`);
+  if(!res.ok) throw new Error(`Unexpected response status: ${res.status}`);
+
+  const hashes = await res.json();
+  const phoneNumber = hashes[rest];
+
+  if(!phoneNumber) throw new Error(`Hash not found.`);
+
+  return phoneNumber;
+};
