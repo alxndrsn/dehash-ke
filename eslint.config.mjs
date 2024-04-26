06b2f62fb9a6aef92f328d6ee5e0c776850eaf90
alxndrsn <alxndrsn>
2024-04-26 14:46:51 +0000

    eslint: add rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,14 @@ export default [
   {
     files: ['**/*.js'],
     languageOptions: { sourceType:'commonjs' },
+    rules: {
+      'eol-last': 'error',
+      'no-tabs': 'error',
+      'no-trailing-spaces': 'error',
+      'no-undef-init': 'error',
+      'no-unused-expressions': 'error',
+      'semi': [ 'error', 'always' ],
+    },
   },
   {
     languageOptions: { globals:globals.node },
